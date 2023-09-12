c9fa757a67374c044a0f9a49cc203c6371556dfc
Phạm Minh Khang <139103727+gamsao3104@users.noreply.github.com>
2023-09-12 16:54:20 +0700

    Add files via upload

---
+++ Slot3/BasicLogic/src/Chao.java
@@ -0,0 +1,21 @@
+
+
+public class Chao {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        int a[] = {1,2,3,4,5};
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + ",");
+        }
+        System.out.println();
+        for(int x:a){
+            System.out.print(x + ",");            
+        }
+        System.out.println();
+        for(int x : a) x+=10;
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + ",");            
+        }
+        System.out.println();
+    }
+}
