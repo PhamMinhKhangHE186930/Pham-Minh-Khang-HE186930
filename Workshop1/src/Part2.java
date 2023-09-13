46e89a134fa04979519a6e1ab3c0c3ca5fd8e117
Phạm Minh Khang <139103727+gamsao3104@users.noreply.github.com>
2023-09-13 12:17:11 +0700

    Add files via upload

--- Workshop1/src/Part2.java
+++ Workshop1/src/Part2.java
@@ -1,16 +1,6 @@
 
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author ADMIN
- */
 public class Part2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
